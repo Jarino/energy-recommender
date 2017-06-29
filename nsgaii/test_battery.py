a89672cc04177d7f109d86baf9f4e5e724410986
Jarino <jaroslavloebl@gmail.com>
2017-06-29 10:34:40 +0200

    unit test for battery class

--- nsgaii/test_battery.py
+++ nsgaii/test_battery.py
@@ -1,10 +1,19 @@
-import numpy as np
 import nsgaii.battery as battery
 
 
 def test_discharge():
-    assert False  # TODO implement
+    btr = battery.Battery(battery_max=3, current_level=2)
+
+    excess = btr.discharge(5)
+
+    assert btr.current_level == 0
+    assert excess == 3
 
 
-def test_recharge():
-    assert False  # TODO implement
+def test_charge():
+    btr = battery.Battery(battery_max=3, current_level=0)
+
+    excess = btr.charge(5)
+
+    assert btr.current_level == 3
+    assert excess == 2
